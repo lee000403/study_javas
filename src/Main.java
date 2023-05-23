1bf4f1c76f9992eac2ef253bd4a4fc684a424b00
lee000403 <dong000403@gmail.com>
2023-05-23 13:09:55 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Leedonghwan";
+        int age = 24;
+        System.out.println(name + " " + age);
+        // return 0;
+    }
+}
