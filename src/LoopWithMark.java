dd1df10892f4b0fbe969b248c22aab4a64b69b3f
lee000403 <dong000403@gmail.com>
2023-05-24 15:24:18 +0900

    gg

--- src/LoopWithMark.java
+++ src/LoopWithMark.java
@@ -1,6 +1,14 @@
 
 public class LoopWithMark {
     public static void main(String[] args) {
+        String str = "for ";
+        int num = 5;
+        for (int i=1; i < num; i = i + 1) {
+            System.out.println(str + i);
+        }
+        System.out.println("end");
+
+
         String s = "?_";
         String a = "?_";
         for (int i= 1; i<=5; i = i+1) {
