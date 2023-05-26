e3699cada2401f2de869cbf0c92a79b40b1c8652
lee000403 <dong000403@gmail.com>
2023-05-26 16:51:06 +0900

    캐셔

---
+++ src/CasherFor.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CasherFor {
+    public static void main(String[] args) {
+        try {
+            Scanner in = new Scanner(System.in);
+            int[] price_sum = {0, 0, 0, 0, 0};
+            int sum = 0;
+            System.out.println("----- 계산 시작 -----");
+
+            for (int count = 0; count < price_sum.length; count = count + 1){
+                System.out.print((count+1)+" 번째 가격 : ");
+                int price = in.nextInt();
+                if (price <= 100 && price > 0) {
+                    price_sum[count] = price;
+                }
+                else if (price > 100) {
+                    price = 0;
+                    price_sum[count] = price;
+                    System.out.println("100 초과 0원 적용");
+                }
+                else if (price == 0){
+                    break;
+                }
+            }
+            for (int col = 0; col < price_sum.length; col = col + 1) {
+                sum = sum + price_sum[col];
+            }
+            System.out.println("----- 담은 총합 -----");
+            System.out.println("합계 : " + sum);
+            System.out.println("----- 계산 종료 -----");
+        } 
+        catch (Exception e) {
+        } 
+        finally {
+        }
+        // return 0;
+    }
+}
---
+++ src/cases/ForBuckets.java
@@ -0,0 +1,30 @@
+package cases;
+
+import java.util.Scanner;
+
+public class ForBuckets {
+    public static void main(String[] args) {
+        try {
+            // 장바구니 3가지만 담게 선언
+            String bucket_1 = "";
+            String bucket_2 = "";
+            String bucket_3 = "";
+            String[] bucket = {"", "", ""};
+
+            Scanner in = new Scanner(System.in);
+            for (int first = 0; first < bucket.length; first++) {
+                System.out.print("1번째 담기 : ");
+                bucket_1 = in.nextLine();
+            }
+
+            System.out.println("---- 담은 내용 ----");
+            System.out.print(bucket_1);
+            System.out.print(bucket_2);
+            System.out.print(bucket_3);
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+            
+        }
+    }
+}
