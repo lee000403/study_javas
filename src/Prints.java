5ef6eff1dec00d324113614897dfd0c2bae2a25d
lee000403 <dong000403@gmail.com>
2023-05-26 13:08:36 +0900

    cases에 옮김

--- src/Prints.java
+++
@@ -1,9 +0,0 @@
-public class Prints {
-    public static void main(String[] args) {
-        String name = "빅데이터 java 교육 과정";
-        System.out.println(name);
-        
-        // return 0;
-        
-    }
-}
