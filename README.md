8e4d30a6c2d04fd203b523ac3ab1a8e985cccc41
lee000403 <dong000403@gmail.com>
2023-05-26 12:58:03 +0900

    완료

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 | Print | Main.java| 화면에 문자 출력 | |
 | If문 | [Ifs.java](./src/Ifs.java) | 조건문 | if () {} 형태 |
 | 데이터 타입 변환 | [TypeCasting.java]([src/TypeCasting.java](https://github.com/lee000403/study_javas/blob/master/src/TypeCasting.java)) | 데이터 타입 변경 | --- |
+| Break | [Breaks.java](./src/Breaks.java) | 루프 탈출 문법 | |
 ## JAVA 연습 문
---
+++ src/PollsWhile.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PollsWhile {
+    public static void main(String[] args) {
+        try {
+            // 스캐너
+            Scanner in = new Scanner(System.in);
+
+            // 안내 글
+            System.out.println("선택에 따라 안내글을 보여드립니다.");
+            System.out.println("------ 작동 key ------");
+            System.out.println("(E)xit : 종료");
+            System.out.println("(P)oll : 설문 시작");
+            System.out.println("(S)tatistic : 설문 통계");
+            System.out.println();
+
+            // 선택 칸
+            while (true) {
+                System.out.print("선택 입력 : ");
+                String s = in.nextLine();
+                if (s.equals("P") || s.equals("Poll")) {
+                    System.out.println("------ 설문 시작 ------");
+                    System.out.println();
+                }
+                else if (s.equals("S") || s.equals("Statistic")) {
+                    System.out.println("------ 설문 통계 ------");
+                    System.out.println();
+                }
+                else if (s.equals("E") || s.equals("Exit")) {
+                    System.out.println("------ 설문 종료 ------");
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
