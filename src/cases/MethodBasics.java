dedfd04b7d33f05d19b24cb275d9e9baf2f35e95
lee000403 <dong000403@gmail.com>
2023-05-30 16:46:02 +0900

    commit

--- src/cases/MethodBasics.java
+++ src/cases/MethodBasics.java
@@ -1,29 +1,34 @@
 package cases;
 
 public class MethodBasics {
-    // x에 + 1
-    // () 가 있으면 function 이다.
-    public int F(int x) {
-        int result = x + 1; // 숫자 결과
-        return result; 
+    // <access_modifier> <return_type> <method_name>( list_of_parameters)
+    // {
+    // //body
+    // }
+    // x에 +1
+    public int F(int x) // x = 0
+    {
+        int result = x + 1;
+        return result;
     }
     public static void main(String[] args) {
         try {
             int x = 4;
             int y = 0;
-            y = x + 1; // 변수에 + 1
-            // y = y + 1; // 변수에 + 1
-            // new : 클래스 전체를 가져온다.
-            MethodBasics methodBasics = new MethodBasics(); // 맨 앞부분은 class 데이터 타입인것을 나타낸다.
-            y = methodBasics.F(2);
-            MethodBasics methodBasics_first = new MethodBasics();
-            y = methodBasics_first.F(3);
-            System.out.println(y);
-        } catch (Exception e) {
-        } finally {
+            // y = x + 1; // 변수에 + 1
+            // y = F(x);
+            MethodBasics methodBasics = new MethodBasics();
+            y = methodBasics.F(x);
 
+            y = y + 1; // 변수에 + 1
+            MethodBasics methodBasics_first = new MethodBasics();
+            y = methodBasics_first.F(y);  // F(0)
+            y = methodBasics_first.F(y);  // F(0)
+
+        } catch (Exception e) {
+            // TODO: handle exception
         }
+        System.out.println();
         // return 0;
     }
-
 }
---
+++ src/cases/Methods.java
@@ -0,0 +1,48 @@
+package cases;
+
+public class Methods {
+    // <access_modifier> <return_type> <method_name>( list_of_parameters)
+    // {
+    // //body
+    // }
+    public int add(int first, int second) {
+        int result = 0;
+        try {
+            result = first + second;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return result;
+    }
+    public int sub(int first, int second) {
+        int result = 0;
+        try {
+            result = first - second;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        // 인스턴스 1회
+        // 초기 값은 2를 넘지 않음.
+        // main 결과값은 17
+        try {
+            Methods methods = new Methods();
+            int result_add = methods.add(2, 2);
+            result_add = result_add + methods.add(2, 2);
+            result_add = result_add + methods.add(2, 2);
+            result_add = result_add + methods.add(2, 2);
+            result_add = result_add + methods.add(2, 2);
+            int result_sub = methods.sub(1, 2);
+            result_sub = result_sub + methods.sub(1, 2);
+            result_sub = result_sub + methods.sub(1, 2);
+            int result = result_add + result_sub;
+            System.out.println(result);
+        } catch (Exception e) {
+        } finally {
+
+        }
+        // return 0;
+    }
+}
