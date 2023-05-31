a10b251124a642232212b58dcdf891dfd30079d4
YebinKim98 <dpqls9898@gmail.com>
2023-05-31 12:18:16 +0900

    git

---
+++ src/yebinkim.java
@@ -0,0 +1,12 @@
+public class yebinkim {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("김예빈입니다.");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //return
+    }
+    
+}
