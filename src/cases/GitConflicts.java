5ac0b9a2910bcc341dc0cf091ec02cf6a66a9f31
lee000403 <dong000403@gmail.com>
2023-05-31 11:37:07 +0900

    second Git

---
+++ src/cases/GitConflicts.java
@@ -0,0 +1,13 @@
+package cases;
+
+public class GitConflicts {
+    public static void main(String[] args) {
+        try {
+            int first= 2;
+            first = first + 1;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+    }
+}
