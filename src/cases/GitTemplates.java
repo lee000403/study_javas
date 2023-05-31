f34fbe99b6628b197e2f827b19e049417a056810
lee000403 <dong000403@gmail.com>
2023-05-31 11:09:21 +0900

    new github second

---
+++ src/cases/GitTemplates.java
@@ -0,0 +1,15 @@
+package cases;
+public class GitTemplates {
+    public static void main(String[] args) {
+        try {
+            int first = 0;
+            int second = 1;
+            second = first + 1;
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+
+        }
+        // return 0;
+    }
+}
